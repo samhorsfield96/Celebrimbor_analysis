9d42978fc5ea09912db85d9b5ee8f67743eaa59a
Samuel Horsfield <shorsfield@ebi.ac.uk>
2024-01-26 11:26:58 +0000

    Updates remove_sequence.py to generate output sequences

--- remove_sequence.py
+++ remove_sequence.py
@@ -4,7 +4,27 @@ import pandas as pd
 import numpy as np
 import os
 from Bio import SeqIO
+from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+
+def remove_ranges_from_sequence(sequence, ranges, length_list):
+    # Join the sequences into one string
+    full_sequence = list(''.join(sequence))
+
+    # Remove the specified ranges
+    for start, end in ranges:
+        full_sequence[start:end] = ["0"] * (end - start)
+
+    # split back up into original contigs
+    list_start = 0
+    result = []
+    for length in length_list:
+        to_append = ''.join(full_sequence[list_start: list_start + length])
+        to_append = to_append.replace("0", "")
+        result.append(to_append)
+        list_start += length
+
+    return result
 
 def get_options():
     description = 'Removes COGs from presence/absence matrix based on epirical distribution of completeness.'
@@ -39,13 +59,18 @@ def main():
     input = options.input
     prop_complete = options.prop_complete
     dist_file = options.dist
-    outdir = options.outpref
+    outdir = options.outdir
     avg_breaks = options.avg_breaks
+
+    # set average breaks to zero to ensure that the number of splits made is equal to that specified
+    avg_breaks -= 1
+    if avg_breaks < 0:
+        avg_breaks = 0
 
     # create output directory
     try:
         os.mkdir(outdir)
-    except FileExistsError:
+    except OSError:
         pass
 
     # read in prop_complete, generate distribution
@@ -58,7 +83,7 @@ def main():
 
     # read in file list
     file_list = []
-    with open(file_list, "r") as f:
+    with open(input, "r") as f:
         for line in f:
             file_list.append(line.rstrip())
 
@@ -68,14 +93,15 @@ def main():
     number_complete = round(num_genomes * prop_complete)
 
     # sample from distribution genomes at random
-    incomplete_sample = np.random.choice(dist_list, size=(num_genomes - number_complete))
+    incomplete_sample = np.random.choice(dist_list, size=(num_genomes - number_complete), replace=True)
     completeness_array = np.full(num_genomes, 1.0)
 
     # # set random isolates to non-complete
-    incomplete_genomes = np.random.choice(len(num_genomes), size=incomplete_sample.size, replace=False)
+    incomplete_genomes = np.random.choice(num_genomes, size=incomplete_sample.size, replace=False)
     completeness_array[incomplete_genomes] = incomplete_sample
 
-    to_remove = {}
+    to_remove_full = []
+    output_file_list = []
 
     # iterate of genomes to augment
     for index, completeness in enumerate(completeness_array):
@@ -85,11 +111,8 @@ def main():
         fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
         output_sequences = []
 
-        extension = "_augmented"
-        if completeness == 1.0:
-            extension = "_complete"
-
         sequence_list = []
+        name_list = []
         length_list = []
 
         for fasta in fasta_sequences:
@@ -100,85 +123,68 @@ def main():
                 output_sequences.append(SeqRecord(Seq(sequence), id=name, description=""))
                 continue
 
-            sequence_list.append((name, sequence))
+            name_list.append(name)
+            sequence_list.append(sequence)
             length_list.append(len(sequence))
         
         # remove sections from genome
+        to_remove = []
         if completeness < 1.0:
             genome_size = sum(length_list)
-            num_breaks = np.random.poisson(lam=avg_breaks, size=1)
+            num_breaks = np.random.poisson(lam=avg_breaks, size=1)[0]
+
+            # testing
+            #num_breaks = 1
+
             size_removal = round(genome_size * (1 - completeness))
 
-            # get sizes of each break
-            break_sites = np.random.choice(size_removal, size=num_breaks, replace=False).tolist()
+            # get sizes of each break. If no breaks then just take one big block
+            if num_breaks > 0:
+                break_sites = np.random.choice(size_removal, size=num_breaks, replace=False).tolist()
             break_sites.extend([0, size_removal])
+
+            # determine size of each break
             break_sizes = np.diff(np.sort(break_sites))
+            np.random.shuffle(break_sizes)
+            sum_breaks = sum(break_sizes)
 
-        SeqIO.write(output_sequences, outdir + basename + extension + ".fasta", 'fasta')
+            # search between start and furthest possible position to remove all blocks
+            start_pos = 0
+            end_pos = genome_size - sum_breaks
+            for block in break_sizes:
+                break_start = np.random.choice(range(start_pos, end_pos), size=1)[0]
+                break_end = break_start + block
+                
+                to_remove.append((break_start, break_end))
+                
+                # update site values
+                start_pos = break_end
+                sum_breaks -= block
+                end_pos = genome_size - sum_breaks
 
+            # determine where contig breaks are
+            # list_A = ['ATGC', 'AATC', 'CCTG']
+            # list_B = [(2, 6), (7, 10)]
+            # length_list = [4, 4, 4]
+            augmented_sequence = remove_ranges_from_sequence(sequence_list, to_remove, length_list)
 
-        # need to know:
-        # - which locations (and therefore genes) have been removed
-        # - which genes are actually core (can get by running celebrimbor without any removal)
+            for name, sequence in zip(name_list, augmented_sequence):
+                if len(sequence) > 0:
+                    #test = len(sequence)
+                    output_sequences.append(SeqRecord(Seq(sequence), id=name, description=""))
+        else:
+            to_remove.append((None, None))
 
-        # pull out current genome data
-        genome_size, gene_pos = gene_coords[genome_index[index]]
-        to_remove_list = []
-
-        # generate blocks to remove
-        size_removal = round(genome_size * (1 - completeness))
-        num_breaks = np.random.poisson(lam=avg_breaks, size=1)
-        break_sites = np.random.choice(size_removal, size=num_breaks, replace=False).tolist()
-        break_sites.extend([0, size_removal])
-        break_sizes = np.diff(np.sort(break_sites))
-
-        # shuffle array to ensure block sizes are randomised
-        np.random.shuffle(break_sizes)
-        sum_breaks = sum(break_sizes)
-
-        # search between start and furthest possible position to remove all blocks
-        start_pos = 1
-        end_pos = (genome_size + 1) - sum_breaks
-        for block in break_sizes:
-            break_start = np.random.choice(range(start_pos, end_pos), size=1)[0]
-            break_end = break_start + block
-
-            #identify genes overlapping in range
-            for CDS_id, CDS_coords in gene_pos.items():
-                CDS_start, CDS_end = CDS_coords
-                gene_length = CDS_end - CDS_start
-                overlap = len(range(max(break_start, CDS_start), min(break_end, CDS_end) + 1))
-                if overlap > (gene_length * (1 - prop_CDS)):
-                    to_remove_list.append(gene_clusters[CDS_id])
-
-            # update site values
-            start_pos += break_end
-            sum_breaks -= block
-            end_pos = (genome_size + 1) - sum_breaks
-
-        to_remove[genome_index[index]] = (completeness, to_remove_list, break_sizes.tolist())
-
-    # update pa_mat
-    for index, genome_id in genome_index.items():
-        completeness, to_remove_list, break_sizes = to_remove[genome_id]
-        # move on if complete
-        if completeness == 1:
-            continue
-
-        row_slice = pa_mat.loc[genome_id, :]
-        for element in to_remove_list:
-            row_slice[element] = 0
-
-    # write removed CDS json
-    with open(outpref + '_removed.json', 'w') as fp:
-        json.dump(to_remove, fp)
-
-    # write updated array
-    pa_mat.to_csv(outpref + "_removed_gene_pa.csv")
-
-    #write completeness statistics
-    completeness_df = pd.DataFrame({'Genome': pa_mat.index, 'Completeness' : completeness_array.tolist()})
-    completeness_df.to_csv(outpref + "_completeness_sim.csv")
+        to_remove_full.append(to_remove)
+        SeqIO.write(output_sequences, outdir + "/" + basename + "_comp_" + str(round(completeness, 4)) + ".fasta", 'fasta')
+        output_file_list.append(basename + "_comp_" + str(round(completeness, 4)) + ".fasta")
+    
+    # output completeness file
+    with open(outdir + "/completeness.tsv", "w+") as f:
+        f.write("File_name\tCompleteness\tRemoved\n")
+        for output_file, completeness, removed in zip(output_file_list, completeness_array, to_remove_full):
+            f.write(output_file + "\t" + str(completeness) + "\t" + ', '.join([f'({item[0]}, {item[1]})' for item in removed]) + "\n")
+            
 
 if __name__ == "__main__":
     main()
